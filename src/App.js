da507f666576f439c28caea91fac22204092247d
Aayush jain <aayushjain1118@gmail.com>
2021-01-09 00:07:15 +0530

    react ui updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { Link, Route, Switch } from 'react-router-dom';
+import { Todolist } from './components/TodoList';
+import { CreateTodo } from './components/CreateTodo';
+import { EditTodo } from './components/EditTodo';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<nav className='navbar bg-light navbar-expand-lg navbar-light'>
+				<ul className='navbar-nav mr-auto px-5'>
+					<li className='nav-item'>
+						<Link to='/' className='nav-link active'>
+							Todos
+						</Link>
+					</li>
+					<li className='nav-item'>
+						<Link to='/create' className='nav-link'>
+							Create
+						</Link>
+					</li>
+				</ul>
+			</nav>
+			<Switch>
+				<Route exact path='/' component={Todolist} />
+				<Route path='/edit:id' component={EditTodo} />
+				<Route path='/create' component={CreateTodo} />
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
